7b7adf143aa5c1567fd08a43820442d544be714e
HamnaFazil <hamnafazil27@gmail.com>
2025-08-30 11:02:28 +0530

    updated

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 javascript
 // Simple console message
+const toggle = document.getElementById('themeToggle');
+toggle.addEventListener('click', () => {
+  document.body.classList.toggle('dark');
+});
 console.log("Hamna's Portfolio Loaded Successfully!");
